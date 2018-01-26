d0c6e5bb8a70b4092f318a6cbd495a0d28cf5222
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 10:43:49 -0500

    Finished coding and formating output Everything works

--- src/main/java/com/example/demo/MagicNumberPattern.java
+++ src/main/java/com/example/demo/MagicNumberPattern.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 public class MagicNumberPattern {
-    public String magicNumbering(){
-        String output ="";
-        Integer input_number;
-
+    public String magicNumbering(Integer input_number){
+        String output = "" + input_number;
+        while (input_number != 1){
+            if(input_number % 2 == 0){
+                input_number = input_number / 2;
+            }else{
+                input_number = input_number * 3 + 1;
+            }
+            output = output + "    " + input_number;
+        }
 
 
         return output;
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,22 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MainController {
     @RequestMapping ("/")
     public String magic_numberer(@RequestParam("number") int input_number){
         MagicNumberPattern magicNumber = new MagicNumberPattern();
-        return magicNumber.magicNumbering(input_number);
+        String result = magicNumber.magicNumbering(input_number);
+        return "Your web page prints:<br/>Starting Number: " + input_number + "<br/>" + result;
 
     }
 }
-/*
-@RequestMapping("/getname")
-    public String showWithName(@RequestParam("county") String name,@RequestParam("age") int age)
-    {
-
-        return name+" "+age;
-    }
-    */
