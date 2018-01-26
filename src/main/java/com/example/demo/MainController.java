756bf20e261a806dbcf067f3a2c5a3a16047ce4e
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 10:17:36 -0500

    ready to test URL input

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -6,5 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RestController
 public class MainController {
+    @RequestMapping ("/")
+    public String magic_numberer(@RequestParam("number") int input_number){
+        MagicNumberPattern magicNumber = new MagicNumberPattern();
+        return magicNumber.magicNumbering(input_number);
+
+    }
 }
+/*
+@RequestMapping("/getname")
+    public String showWithName(@RequestParam("county") String name,@RequestParam("age") int age)
+    {
+
+        return name+" "+age;
+    }
+    */
