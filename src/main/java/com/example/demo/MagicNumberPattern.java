829046728cd8d70a83cb55e4e5768e49a2565f58
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 11:20:52 -0500

    created a modified branch out of boredom

--- src/main/java/com/example/demo/MagicNumberPattern.java
+++ src/main/java/com/example/demo/MagicNumberPattern.java
@@ -3,14 +3,19 @@ package com.example.demo;
 public class MagicNumberPattern {
     public String magicNumbering(Integer input_number){
         String output = "" + input_number;
-        while (input_number != 1){
-            if(input_number % 2 == 0){
-                input_number = input_number / 2;
-            }else{
-                input_number = input_number * 3 + 1;
-            }
-            output = output + "    " + input_number;
+        if (input_number <= 0) {
+            output = "Why did you put in a number less than 1?";
         }
+        else{
+            while (input_number != 1) {
+                if (input_number % 2 == 0) {
+                    input_number = input_number / 2;
+                } else {
+                    input_number = input_number * 3 + 1;
+                }
+                output = output + "    " + input_number;
+                }
+            }
 
 
         return output;
