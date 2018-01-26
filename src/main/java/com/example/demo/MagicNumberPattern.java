b2793d919cf7c3abd5da0ed278ee0e9c73796e5e
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 09:58:46 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MagicNumberPattern.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class MagicNumberPattern {
+    public String magicNumbering(){
+        String output ="";
+        Integer input_number;
+
+
+
+        return output;
+    }
+}
---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MainController {
+}
